8b5da2f7884c6f63a82f75f45cdde2b3b55e2bb8
vaibhav kamade <vaibhavkamade20@gmail.com>
2024-01-15 00:49:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+var timer = 60;
+var score = 0;
+var hitrn = 0;
+
+function makeBubble(){
+
+    var clutter = "";
+    for(var a = 0 ; a < 102 ; a++){
+        var rn = Math.floor(Math.random() * 10);
+        clutter += `<div class="bubble">${rn}</div>`
+    }
+
+    document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+function runTimer(){
+
+   var timeID = setInterval(function (){
+        if(timer > 0 ){
+            timer--;
+            document.querySelector("#runtime").textContent = timer;
+        }else{
+            clearInterval(timeID);
+            document.querySelector("#pbtm").innerHTML =  `<div><h1> Game Over !!</h1> </br> <h1> Your Score: ${score}</h1></div>`;
+
+        }
+    }, 1000);
+}
+
+function getNewHit(){
+    hitrn = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent = hitrn;
+}
+
+function increaseScore(){
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+}
+
+document.querySelector("#pbtm").addEventListener("click",function(details){
+    var hit = Number(details.target.textContent);
+    if(hit === hitrn){
+        increaseScore();
+        makeBubble();
+        getNewHit();
+    }
+
+})
+
+
+makeBubble();
+runTimer();
+getNewHit();
